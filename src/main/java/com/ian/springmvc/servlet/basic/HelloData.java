98e305070188a2d6e3dd5478190dc29945e25f29
XXN <jaechan27.0.2.28@gmail.com>
2026-02-02 17:39:31 +0900

    18. HTTP 요청 데이터 - API 메시지 바디 - JSON

---
+++ src/main/java/com/ian/springmvc/servlet/basic/HelloData.java
@@ -0,0 +1,13 @@
+package com.ian.springmvc.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+}

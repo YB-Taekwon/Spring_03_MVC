b0f5cbba2cc1adb489719f600d9e97f31e4d1b08
XXN <jaechan27.0.2.28@gmail.com>
2026-02-05 18:08:25 +0900

    57. HTTP 메시지 컨버터

--- SpringMvcProject/src/main/java/com/ian/springmvcproject/basic/response/ResponseBodyController.java
+++ SpringMvcProject/src/main/java/com/ian/springmvcproject/basic/response/ResponseBodyController.java
@@ -13,6 +13,27 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.io.IOException;
 
+/**
+ * 스프링 부트 기본 MessageConverter 동작 순서
+ * 1. ByteArrayHttpMessageConverter -> 읽기: byte[], 쓰기: application/octet-stream
+ * 2. StringHttpMessageConverter -> 읽기: String, 쓰기: text/plain
+ * 3. MappingJackson2HttpMessageConverter -> 읽기: json, 쓰기: application/json
+ * ...
+ * <p>
+ * HTTP 요청 데이터 읽기
+ * 1. 요청이 들어오면 컨트롤러에서 @RequestBody, HttpEntity(RequestEntity) 사용
+ * 2. 메시지 컨버터가 메시지을 읽을 수 있는지 확인 (canRead() 호출)
+ *     - 대상 클래스 타입을 지원하는지 확인
+ *     - HTTP 요청의 Content-Type 미디어 타입을 지원하는지 확인
+ * 3. canRead() 조건을 만족하면 read() 메서드를 호출하여 객체 생성 및 반환
+ * <p>
+ * HTTP 응답 데이터 생성
+ * 1. 컨트롤러에서 @ResponseBody, HttpEntity(ResponseEntity)로 값이 변환됨
+ * 2. 메시지 컨버터가 메시지를 쓸 수 있는지 확인 (canWrite() 호출)
+ *     - 대상 클래스 타입을 지원하는지 확인
+ *     HTTP 요청의 Accept 미디어 타입을 지원하는지 확인
+ * 3. canWrite() 조건을 만족하면 write() 메서드를 호출하여 HTTP 응답 메시지 바디에 데이터를 생성
+ */
 @Slf4j
 @Controller
 public class ResponseBodyController {
